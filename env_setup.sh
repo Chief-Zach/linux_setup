b12df03510be919ffb2162b875a97ff34796ff9c
Anon <github@sudomail.com>
2025-02-11 01:34:36 -0500

    Initial

---
+++ env_setup.sh
@@ -0,0 +1,35 @@
+#!/bin/zsh
+
+
+sudo apt install zsh npm neovim
+
+# Clone PowerLevel
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+
+# Setup PowerLevel
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+echo "Enter the username for the user to activate ZSH for: "
+read username
+
+sudo chsh -s /bin/zsh $username
+
+mkdir -p ~/.config/nvim/bundle
+
+git clone https://github.com/neovim/nvim-lspconfig.git ~/.config/nvim/bundle/nvim-lspconfig
+git clone https://github.com/hrsh7th/nvim-cmp.git ~/.config/nvim/bundle/nvim-cmp
+git clone https://github.com/hrsh7th/cmp-buffer.git ~/.config/nvim/bundle/cmp-buffer
+git clone https://github.com/hrsh7th/cmp-path.git ~/.config/nvim/bundle/cmp-path
+git clone https://github.com/hrsh7th/cmp-nvim-lsp.git ~/.config/nvim/bundle/cmp-nvim-lsp
+git clone https://github.com/vim-airline/vim-airline.git ~/.config/nvim/bundle/vim-airline
+git clone https://github.com/vim-airline/vim-airline-themes.git ~/.config/nvim/bundle/vim-airline-themes
+
+
+wget -O ~/.zshrc https://GITLINK
+
+wget -O ~/.config/nvim/init.lua
+
+npm install -g pyright
+
+nvim -es -c :PlugInstall
+
---
+++ init.lua
@@ -0,0 +1,103 @@
+vim.cmd([[
+call plug#begin('~/.config/nvim/plugged')
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+Plug 'vim-airline/vim-airline'
+Plug 'vim-airline/vim-airline-themes'
+
+" For vsnip users.
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
+call plug#end()
+]])
+
+vim.opt.tabstop = 4       -- Number of spaces for a tab character
+vim.opt.shiftwidth = 4    -- Number of spaces for each indentation level
+vim.opt.expandtab = true  -- Use spaces instead of tab characters
+vim.opt.softtabstop = 4   -- Number of spaces a Tab key press represents in insert mode
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+local cmp = require('cmp')
+
+local select_opts = {behavior = cmp.SelectBehavior.Select}
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+        vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+    end
+  },
+  completion = {
+    max_item_count = 10,  -- Set the maximum number of suggestions to show
+  },
+
+  sources = {
+    {name = 'path'},
+    {name = 'nvim_lsp', keyword_length = 1},
+    {name = 'buffer', keyword_length = 3},
+  },
+  window = {
+    documentation = cmp.config.window.bordered()
+  },
+  formatting = {
+    fields = {'menu', 'abbr', 'kind'},
+    format = function(entry, item)
+      local menu_icon = {
+        nvim_lsp = 'λ',
+        buffer = 'Ω',
+        path = '🖫',
+      }
+
+      item.menu = menu_icon[entry.source.name]
+      return item
+    end,
+  },
+  mapping = {
+    ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+    ['<C-p>'] = cmp.mapping.select_prev_item(select_opts),
+    ['<C-n>'] = cmp.mapping.select_next_item(select_opts),
+
+    ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-d>'] = cmp.mapping.scroll_docs(4),
+
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<C-y>'] = cmp.mapping.confirm({select = true}),
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      local col = vim.fn.col('.') - 1
+
+      if cmp.visible() then
+        cmp.select_next_item(select_opts)
+      elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+        fallback()
+      else
+        cmp.complete()
+      end
+    end, {'i', 's'}),
+
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item(select_opts)
+      else
+        fallback()
+      end
+    end, {'i', 's'}),
+  },
+})
+
+local lspconfig = require('lspconfig')
+
+local servers = { 'clangd', 'pyright' }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end
